6914953e4cd37367ad88b46d4b450296438fdb4d
Jorge <j_masson_reynoso@hotmail.com>
2021-04-23 07:05:20 -0700

    Inicio de sesión

---
+++ app/src/main/java/nieblas_masson/julio_jorge/muno/IniciarSesionActivity.kt
@@ -0,0 +1,11 @@
+package nieblas_masson.julio_jorge.muno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IniciarSesionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion)
+    }
+}
