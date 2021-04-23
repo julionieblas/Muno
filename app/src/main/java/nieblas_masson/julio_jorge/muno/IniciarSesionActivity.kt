eb1fe8607fa4e77362842b9669d810d45d70ace9
Jorge <j_masson_reynoso@hotmail.com>
2021-04-23 07:21:07 -0700

    Correcciones en activity_main y navegación entre pantallas

--- app/src/main/java/nieblas_masson/julio_jorge/muno/IniciarSesionActivity.kt
+++ app/src/main/java/nieblas_masson/julio_jorge/muno/IniciarSesionActivity.kt
@@ -1,11 +1,21 @@
 package nieblas_masson.julio_jorge.muno
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class IniciarSesionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
+
+        val textView: TextView = findViewById(R.id.tv_iniSesion_olvidarContra) as TextView
+
+        textView.setOnClickListener {
+            var i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
+
     }
 }
--- app/src/main/java/nieblas_masson/julio_jorge/muno/MainActivity.kt
+++ app/src/main/java/nieblas_masson/julio_jorge/muno/MainActivity.kt
@@ -1,11 +1,21 @@
 package nieblas_masson.julio_jorge.muno
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById(R.id.et_registro_cuentaExistente) as TextView
+
+        textView.setOnClickListener {
+            var i = Intent(this, IniciarSesionActivity::class.java)
+            startActivity(i)
+        }
+
     }
 }
