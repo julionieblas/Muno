9d92ffab59807734ad226f19a585cbed2b6042e1
JorgeMasson <j_masson_reynoso@hotmail.com>
2021-05-11 05:40:30 -0700

    Perfil

---
+++ app/src/main/java/nieblas_masson/julio_jorge/muno/PerfilActivity.kt
@@ -0,0 +1,11 @@
+package nieblas_masson.julio_jorge.muno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
